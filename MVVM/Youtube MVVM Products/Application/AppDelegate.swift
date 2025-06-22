1b1e937db6e51741397eeb07fc079be43de85dc6
kollurukeerthana <Kolluru.Keerthana@laminaar.in>
2025-06-22 23:27:44 +0530

    ramu added in app

--- MVVM/Youtube MVVM Products/Application/AppDelegate.swift
+++ MVVM/Youtube MVVM Products/Application/AppDelegate.swift
@@ -10,8 +10,7 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    // test app delagte keer branch
-
+    // ramu
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
